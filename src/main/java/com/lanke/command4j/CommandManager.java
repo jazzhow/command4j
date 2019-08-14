42c5f13c340dd461b1a30505f3acb2483c919cca
jazzhow <lfzihao@qq.com>
2019-08-14 12:06:41 +0800

    1.0-RELEASE

---
+++ src/main/java/com/lanke/command4j/CommandManager.java
@@ -0,0 +1,97 @@
+package com.lanke.command4j;
+
+import com.lanke.command4j.model.MyProcess;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class CommandManager {
+    private static final Logger LOGGER = LoggerFactory.getLogger(CommandManager.class);
+    private ConcurrentHashMap<String, MyProcess> processMap = new ConcurrentHashMap<>();
+
+    public CommandManager() {
+        Runtime.getRuntime().addShutdownHook(new Thread(this::destroyAll));
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        this.destroyAll();
+        super.finalize();
+    }
+
+    /**
+     * 使用代码同步防止同一id下的命令启动多次
+     *
+     * @param processId
+     * @param command
+     * @return
+     */
+    public synchronized void exec(String processId, String command) throws IOException {
+        //先判断是否已存在此程序
+        if (getProcess(processId) == null) {
+            Process process = Runtime.getRuntime().exec(command);
+            MyProcess myProcess = new MyProcess(processId, process, processMap);
+            processMap.put(processId, myProcess);
+            LOGGER.info("已启动程序" + processId);
+        } else {
+            LOGGER.warn("已经存在此id " + processId + "对应的程序，请更换id再启动");
+        }
+    }
+
+    /**
+     * 停止任务
+     *
+     * @param processId
+     * @return
+     */
+    public synchronized void destroy(String processId) {
+        LOGGER.info("正在关闭程序" + processId);
+        MyProcess myProcess = processMap.get(processId);
+        if (myProcess != null) {
+            myProcess.setNormalExit(true);
+            Process process = myProcess.getProcess();
+            if (process.isAlive()) {
+                process.destroy();
+                processMap.remove(processId);
+            } else {
+                LOGGER.info("该程序程序" + processId + "已处于关闭状态，无需再次关闭");
+            }
+        } else {
+            LOGGER.info("无法关闭不存在的程序" + processId);
+        }
+    }
+
+    /**
+     * 停止全部任务
+     *
+     * @return
+     */
+    public synchronized int destroyAll() {
+        ArrayList<String> ids = new ArrayList<>();
+        processMap.forEach((processId, myProcess) -> {
+            myProcess.getProcess().destroy();
+            ids.add(processId);
+        });
+        for (String id : ids) {
+            processMap.remove(id);
+        }
+        return ids.size();
+    }
+
+
+    public MyProcess getProcess(String processId) {
+        MyProcess myProcess = processMap.get(processId);
+        return myProcess;
+    }
+
+
+    public Collection<MyProcess> getAllProcess() {
+        Collection<MyProcess> values = processMap.values();
+        return values;
+    }
+
+}
---
+++ src/main/java/com/lanke/command4j/model/MyProcess.java
@@ -0,0 +1,122 @@
+package com.lanke.command4j.model;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class MyProcess {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MyProcess.class);
+    private volatile boolean normalExit = false;
+    private Process process;
+    private String processId;
+    private Thread errInput;
+    private Thread stdInput;
+
+    public MyProcess(String processId, Process process, ConcurrentHashMap<String, MyProcess> processMap) {
+        this.processId = processId;
+        this.process = process;
+        BufferedReader errBfReader = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+        Thread errBfReaderThread = new Thread(() -> {
+            try {
+                String line;
+                while (true) {
+                    try {
+                        if ((line = errBfReader.readLine()) == null) {
+                            break;
+                        }
+                        LOGGER.debug(line);
+                    } catch (IOException e) {
+                        LOGGER.error(e.getMessage(), e);
+                    }
+                }
+            } finally {
+                if (errBfReader != null) {
+                    try {
+                        errBfReader.close();
+                    } catch (IOException e) {
+                        LOGGER.error(e.getMessage(), e);
+                    }
+                }
+
+                if (!process.isAlive()) {
+                    processMap.remove(processId);
+                    int i = process.exitValue();
+                    if (normalExit) {
+                        LOGGER.error("程序" + processId + "运行完毕，返回代码: " + i);
+                    } else {
+                        LOGGER.error("程序" + processId + "被外界环境关闭，返回代码: " + i);
+                    }
+                }
+            }
+        });
+        BufferedReader stdBfReader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        Thread stdBfReaderThread = new Thread(() -> {
+            try {
+                String line;
+                while (true) {
+                    try {
+                        if ((line = stdBfReader.readLine()) == null) {
+                            break;
+                        }
+                        LOGGER.debug(line);
+                    } catch (IOException e) {
+                        LOGGER.error(e.getMessage(), e);
+                    }
+                }
+            } finally {
+                if (stdBfReader != null) {
+                    try {
+                        stdBfReader.close();
+                    } catch (IOException e) {
+                        LOGGER.error(e.getMessage(), e);
+                    }
+                }
+            }
+        });
+        errBfReaderThread.setName("errBfReaderThread-" + processId);
+        stdBfReaderThread.setName("stdBfReaderThread-" + processId);
+        errBfReaderThread.start();
+        stdBfReaderThread.start();
+    }
+
+    public void setNormalExit(boolean normalExit) {
+        this.normalExit = normalExit;
+    }
+
+    public Process getProcess() {
+        return process;
+    }
+
+    public void setProcess(Process process) {
+        this.process = process;
+    }
+
+    public String getProcessId() {
+        return processId;
+    }
+
+    public void setProcessId(String processId) {
+        this.processId = processId;
+    }
+
+    public Thread getErrInput() {
+        return errInput;
+    }
+
+    public void setErrInput(Thread errInput) {
+        this.errInput = errInput;
+    }
+
+    public Thread getStdInput() {
+        return stdInput;
+    }
+
+    public void setStdInput(Thread stdInput) {
+        this.stdInput = stdInput;
+    }
+
+}
